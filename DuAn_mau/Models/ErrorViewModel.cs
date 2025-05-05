c4fbce0d8a33c694412305ee6f4bee6fc8100a72
ADMIN <ADMIN@PC316>
2025-05-05 11:33:41 +0700

    Add project files.

---
+++ DuAn_mau/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DuAn_mau.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
