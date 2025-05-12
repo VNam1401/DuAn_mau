bb9e4e79d89247a0ece7ffb7f451598a38ef55d2
ADMIN <ADMIN@PC316>
2025-05-12 11:32:41 +0700

    002

---
+++ DuAn_mau/Controllers/RegisterController.cs
@@ -0,0 +1,42 @@
+﻿using DuAn_mau.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DuAn_mau.Controllers
+{
+    public class RegisterController : Controller
+    {
+        private IHostingEnvironment hosting;
+        public RegisterController(IHostingEnvironment _hosting)
+        {
+            hosting = _hosting;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult XuLy(PersonViewModel m, IFormFile FHinh)
+        {
+            if (FHinh != null)
+            {
+                //xử lý upload
+                //string filename = FHinh.FileName;
+                string filename = Guid.NewGuid().ToString() + Path.GetExtension(FHinh.FileName);
+                string path = Path.Combine(hosting.WebRootPath, "images");
+                using (var filestream = new FileStream(Path.Combine(path, filename), FileMode.Create))
+                {
+                    //sao chép lên server
+                    FHinh.CopyTo(filestream);
+                }
+                m.Picture = filename;
+            }
+            return View(m);
+        }
+    }
+}
